85c1f8b03999e1ca262566676d2aad02d9bcce79
joshua <joshuaaguilar20@gmail.com>
2019-03-27 19:20:02 -0700

    auto commits rule

--- script.sh
+++ script.sh
@@ -6,4 +6,69 @@ rm -rf removeme.js
 git add . 
 git commit -m "removing"
 git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+touch removeme.js 
+git add . 
+git commit -m "auto commits rule" 
+git push -u origin master 
+rm -rf removeme.js
+git add . 
+git commit -m "removing"
+git push -u origin master
+
 
